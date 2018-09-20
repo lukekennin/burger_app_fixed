d16fee492abc44ea909d706374673e30fe7712e0
Luke Kennin <lkmeq@me.com>
2018-09-20 14:32:46 -0500

    db setup

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,8 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "ly3wtsi35e7s9pqp",
-    password: "zyij1iodgug04m8c",
+    user: "ogghgkf2wn8fa96v",
+    password: "t71y9owg01kx34kw",
     database: "burgers_db"
   });
 }
